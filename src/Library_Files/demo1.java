273c04e67ac514054ec04db0cebd7d8761d28a0b
mypcd <mypcd@DESKTOP-A6LAN9F>
2022-07-31 23:21:02 +0530

    [vinayak] 1st change in a project

---
+++ src/Library_Files/demo1.java
@@ -0,0 +1,11 @@
+package Library_Files;
+
+public class demo1 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hellow Vinayak");
+		
+	}
+
+}
---
+++ src/Library_Files/demo2.java
@@ -0,0 +1,12 @@
+package Library_Files;
+
+public class demo2 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello Vinayak Mane");
+		
+		
+	}
+
+}
